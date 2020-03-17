fbf035e0360fbce170490f4ff50e93a45fb1130f
Maksym Berezovskyi <maksymbe@interlink-ua.com>
2020-03-17 17:34:30 +0200

    Make urls really configurable :)

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,2 @@
-export const XERO_API_BASE_URL = 'https://api.xero.com';
-export const ISSUER_BASE_URL = 'https://identity.xero.com';
+export const XERO_API_BASE_URL = process.env.XERO_API_BASE_URL || 'https://api.xero.com';
+export const ISSUER_BASE_URL = process.env.ISSUER_BASE_URL || 'https://identity.xero.com';
